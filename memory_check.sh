fc4750fb616e5ba93a8d923a3d3b9f237da5e0f2
Aejie23 <iamjhie@gmail.com>
2017-06-10 20:17:23 +0800

    Create memory_check.sh

--- memory_check.sh
+++ memory_check.sh
@@ -4,21 +4,26 @@
 
 #prints total free memory usages
 total_Mem= $(free |grep Mem: |awk '{ print $2 }'| grep % | grep -v Use | sort -n |cut -d '%' -f1)
+top_mem= $(top |grep Mem: | awk '{print $3}' | grep % | grep -v Use | sort -n | cut -d "%" -f1)
+TIME=$(date +%k%M)
+DAY=`/bin/date +%Y%m%d`
 
 #sssss
-while getopts :c:w:z: $total_Mem; do
+while getopts :c:w:z $total_Mem; do
 	case $total_Mem in
 		
-		c) ["$total_Mem" -ge "90"]  
-		exit
+		c) [\("$total_Mem" -ge "90"] && \ ("$total_Mem" -lt "90")] 
+		exit 1
 		;;		
 		
-		w) [ \("$total_Mem" -ge "60" ) && \ ("$total_Mem" -lt "60")]
-		exit
+		w) [\("$total_Mem" -ge "60" ) && \ ("$total_Mem" -lt "60")]
+		exit 1
 		;;
 				
-		z) ["$total_Mem" -lt "90"]
-		exit
+		z) [\("$total_Mem" -ge "90"]
+		
+		echo $top_mem | mail -s " '$DAY' '$TIME' memory check - critical" $z
+		
 		;;
 		
 		?) echo "Please enter required parameter, from 0-100"
