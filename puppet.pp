964a6aa3409f2ac077785eeb3c27172105226a47
Aejie23 <iamjhie@gmail.com>
2017-06-11 00:27:07 +0800

    Create puppet.pp

---
+++ puppet.pp
@@ -0,0 +1,42 @@
+class puppet{
+
+	
+  	include ::wget
+
+	$pack = ['vim','curl','git']
+  		package { $pack: ensure => 'installed' }
+
+	user { 'monitor':
+		ensure	=> 'present',
+		home	=> '/home/monitor',
+		shell	=> '/bin/bash',}
+
+	exec { 'mkdir':
+    	command => 'mkdir /home/monitor/scripts/memory_check',
+	path    => '/usr/local/bin/:/bin/',
+	}
+
+
+    		wget::fetch { 'https://raw.githubusercontent.com/Aejie23/bashScripting/master/memory_check.sh':
+    		destination => '/home/monitor/scripts/memory_check',
+    		timeout     => 15,
+    		verbose     => true,
+		}
+
+	
+	file { '/home/monitor/src/':
+    	ensure	=> 'directory',}
+
+	file { 'my_memory_check':
+    	path	=> '/home/monitor/src/'
+	ensure	=> 'link',
+	target	=> '/home/monitor/scripts/memory_check'}
+
+	cron { 'my_memory_check':
+  	command => '/home/monitor/src/my_memory_check',
+  	user    => 'monitor,
+  	hour    => 0,
+  	minute  => 10,}
+
+
+}
